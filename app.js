b74e9257a27b17fc56b185377a5c02b0fe783c99
ethanz55 <ethanz2000@gmail.com>
2018-03-01 20:07:07 -0800

    added 330px media query and a11y

--- app.js
+++ app.js
@@ -6,40 +6,14 @@ function getDataFromApi(searchTerm, callback) {
     lat: searchTerm.coords.latitude,
     lon: searchTerm.coords.longitude
   }
-  // $.getJSON(zomatoEndpoint, query, callback);
-  
   
   fetch(zomatoEndpoint + '?lat=' + query.lat + '&lon=' + query.lon, {
     headers: {'user-key': '79f6bb0db344b1df724ab923597bc410'}
   }).then(res => res.json()).then(callback);
   
   console.log(zomatoEndpoint + '?lat=' + query.lat + '&lon=' + query.lon);
-  /**({
-    url: zomatoEndpoint,
-    type: 'GET',
-    datatype: 'json',
-    success: callback,
-    beforeSend: setHeader
-  })
-  **/
 }
 
-/**
-        $.ajax({
-          url: 'service.svc/Request',
-          type: 'GET',
-          dataType: 'json',
-          success: function() { alert('hello!'); },
-          error: function() { alert('boo!'); },
-          beforeSend: setHeader
-        });
-      });
-
-      function setHeader(xhr) {
-        xhr.setRequestHeader('securityCode', 'Foo');
-        xhr.setRequestHeader('passkey', 'Bar');
-      }
-**/
 
 function displayZomatoResults(data) {
   const display = data.nearby_restaurants.map((item,index) => {
@@ -48,21 +22,20 @@ function displayZomatoResults(data) {
     // let streetAddress = ${rest.location.address}.split(" ");
     // console.log(streetAddress);
     return `
-      <main>
         <section class="resultsContainer">
-          <img src="http://via.placeholder.com/310x200" class="thumbnailSize">
+          <img src="https://aquaair-wetdry.com/wp-content/uploads/CroppedImage1600900-restaurant-alpenhof-zermatt.jpg" class="thumbnailSize" alt="picture of the restaurant">
           <ul>
-            <li id="no-list">
-              <h2><a href="#">${rest.name}</a></h2>
+            <li class="no-list">
+              <h2><a href="${rest.url}" target="_blank">${rest.name}</a></h2>
             </li>
             <li>${rest.cuisines}</li>
             <li>${rest.user_rating.aggregate_rating}/5 stars &#8226; ${rest.user_rating.votes} votes</li>
             <li>600 N. Baldwin Ave</li>
             <li>${rest.location.city} ${rest.location.zipcode}</li>
             <li>(212) 228-2930</li>
+            <li>Average Cost For Two: &#36;${rest.average_cost_for_two}</li>
           </ul>
         </section>
-      </main>
     `;
   });
   // split address at comma
@@ -75,6 +48,8 @@ function displayZomatoResults(data) {
 
 function eventListeners() {
   $('.js-button').on('click', function(event) {
+    var snd = new Audio("buttonPress.mp3");
+    snd.play();
     $('header').addClass('clickHeader');
     $('.js-button').addClass('clickButton');
     $('.slogan').css('display', 'block');
